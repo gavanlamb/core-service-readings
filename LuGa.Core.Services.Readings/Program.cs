7f418062f4804920f4eb21bcced88fdde80be2ab
Gavan Lamb <gavan.jon.lamb@gmail.com>
2018-02-26 00:37:24 +1100

    Initial commit

---
+++ LuGa.Core.Services.Readings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuGa.Core.Services.Readings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
