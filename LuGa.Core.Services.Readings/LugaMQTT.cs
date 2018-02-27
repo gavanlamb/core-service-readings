cdf011e329ebba7c44fffd1c9c9265d2ef680131
lemining <luke@lewandowskis.com>
2018-02-27 21:58:02 +1100

    Added necessary mqtt code - very basic

---
+++ LuGa.Core.Services.Readings/LugaMQTT.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using LuGa.Core.Device.Models;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.ManagedClient;
+using Topshelf;
+
+namespace LuGa.Core.Services.Readings
+{
+    public class LuGaMqtt : ServiceControl
+    {
+        private readonly IManagedMqttClient _client;
+
+        public LuGaMqtt()
+        {
+
+            var factory = new MqttFactory();
+
+            var options = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(new MqttClientOptionsBuilder()
+                    .WithClientId("reader")
+                    .WithTcpServer("mqtt.luga.online", 1883)
+                    .WithCredentials("reader", "xxx")
+                    .Build())
+                .Build();
+
+            _client = new MqttFactory().CreateManagedMqttClient();
+
+
+            _client.ApplicationMessageReceived += (s, e) =>
+            {
+                if (e.ApplicationMessage.Topic.IndexOf("/value", StringComparison.Ordinal) <= -1 ||
+                    e.ApplicationMessage.Topic.IndexOf("$", StringComparison.Ordinal) != -1) return;
+
+                var reading = new Reading();
+                var pulled = e.ApplicationMessage.Topic.Split("/");
+
+                reading.DeviceId = pulled[1];
+                reading.ReadingType = pulled[2];
+                reading.Value = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                reading.TimeStamp = DateTime.UtcNow;
+
+                Console.WriteLine();
+
+            };
+
+            _client.Connected += async (s, e) =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+                await _client.SubscribeAsync(new TopicFilterBuilder().WithTopic("devices/#").Build());
+            };
+
+            _client.Disconnected += async (s, e) => { Console.WriteLine("### DISCONNECTED WITH SERVER ###"); };
+
+            Task.Run(() => Background(options));
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("Started the service");
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("Stopped the service");
+
+            return true;
+        }
+
+        async Task Background(ManagedMqttClientOptions options)
+        {
+            await _client.StartAsync(options);
+        }
+    }
+}
--- LuGa.Core.Services.Readings/Program.cs
+++ LuGa.Core.Services.Readings/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.ManagedClient;
 
 namespace LuGa.Core.Services.Readings
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Topshelf;
+
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return (int)HostFactory.Run(x =>
+            {
+                x.Service<LuGaMqtt>();
+                
+                x.SetServiceName("luga-reader");
+                x.SetDisplayName("luga-reader");
+            });
         }
     }
+
 }
